19b287aae095b845b2e83840c5ce1c292898095b
shun-tak <shun.tak221@gmail.com>
2014-01-09 20:34:36 +0900

    第2章の理解度チェック

---
+++ chapter2/check2.3.cpp
@@ -0,0 +1,18 @@
+/* 以下のプログラムを実行すると、
+   10 1000000 -0.0009
+   と表示される。
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i = 10;
+  long l = 1000000;
+  double d = -0.0009;
+
+  cout << i << ' ' << l << ' ' << d;
+  cout << "\n";
+
+  return 0;
+}
